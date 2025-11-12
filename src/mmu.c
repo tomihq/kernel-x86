0be6248ddfc22cc7bb92d2b5f3373ef5b4556ec5
tomihq <hernandeztomas584@gmail.com>
2025-11-12 13:51:38 -0300

    add: fin malloco

--- src/mmu.c
+++ src/mmu.c
@@ -259,7 +259,7 @@ bool page_fault_handler(vaddr_t virt) {
           paddr_r phy = next_free_user_page();
           zero_page(phy);
           mmu_map_page(virt, phy);
-          reservas_por_tarea* reservas = dameReservas(ENVIRONMENT -> task_id);
+          reservas_por_tarea* reservas = dameReservas(current_task);
           for(int i = 0; i< reservas -> reservas_size; i++){
             reserva_t reserva = reservas -> array_reservas[i];
             if(reserva -> virt == virt){
@@ -267,7 +267,7 @@ bool page_fault_handler(vaddr_t virt) {
             }
           }
       }else{
-           reservas_por_tarea* reservas = dameReservas(ENVIRONMENT -> task_id);
+           reservas_por_tarea* reservas = dameReservas(current_task);
            for(int i = 0; i<reservas -> reservas_size; i++){
               reserva_t reserva = reservas -> array_reservas[i];
               reserva -> estado = 2; 
@@ -309,7 +309,7 @@ uint32_t get_cr3_by_selector(int16_t selector){
 // Necesito sumar todos los tamanio de reserva_t obteniendo primero reservas_por_tarea* de dameReservas.
 // El area de memoria virtual que tengo que reservar es: direccionVirtualMaxReservada + tamanioReserva. A partir de ESA dirección ocupo el size que me pasan. 
 void* malloco(size_t size){
-  reservas_por_tarea* reservas = sched_tasks[ENVIRONMENT -> task_id];
+  reservas_por_tarea* reservas = sched_tasks[current_task];
   uint32_t tamanio_maximo_reservado = 0; 
   uint32_t maxReservada = 0; 
   for(int i = 0; i < reservas -> reservas_size; i++){
@@ -329,8 +329,18 @@ void* malloco(size_t size){
 
   return (void*) maxReservada; 
 }
-
+//estoy en contexto de tarea tengo CR3 y todo.
+//Lo unico que tengo que hacer acá es decirle al array de tareas, buscar el que coincide con el virt y ponerlo en estado 2 para que despues el garbage collector haga su laburo (desmapear.)
+//por enunciado, me mandan el virt que deberia ser el virt de una reserva_t.
+//si me mandan nada que ver en virt el comportamiento es indefinido
 void chau(vaddr_t virt){
   if(esMemoriaReservada(virt) == 0) return; 
+  reservas_por_tarea* reservas = dameReservas(current_task);
+  for(int i = 0; i<reservas -> reservas_size; i++){
+    reserva_t reserva = reservas -> array_reservas[i];
+    if(reserva -> virt == virt){
+      reserva -> estado = 2; 
+    }
+  }
 
 }
